b9b45e3dd044148f7336ef9484b8bff124179301
wangxin20050524 <wangxin20050524@163.com>
2014-02-22 18:22:32 +0800

    init

---
+++ src/com/mike/gradle/java/demo/GradeJavaMain.java
@@ -0,0 +1,10 @@
+package com.mike.gradle.java.demo;
+
+public class GradeJavaMain {
+	
+	public static void main(String[] args) {
+		//do nothings
+		System.out.println("This is JavaGradle Demo");
+	}
+
+}
